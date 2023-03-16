c1454273006fffdf0621d7730758fc507aac0109
eltoro0815 <th_ru@web.de>
2023-03-16 23:02:35 +0100

    added https://github.com/chiiya/filament-access-control

--- config/filament.php
+++ config/filament.php
@@ -85,9 +85,9 @@ return [
     */
 
     'auth' => [
-        'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
+        'guard' => env('FILAMENT_AUTH_GUARD', 'filament'),
         'pages' => [
-            'login' => \Filament\Http\Livewire\Auth\Login::class,
+            'login' => \Chiiya\FilamentAccessControl\Http\Livewire\Login::class,
         ],
     ],
 
