538bca9c8e5d2a5128ef510048ce0e35c968943a
eltoro0815 <th_ru@web.de>
2023-03-18 16:06:07 +0100

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Chiiya\FilamentAccessControl\Enumerators\RoleName;
+use Chiiya\FilamentAccessControl\Models\FilamentUser;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $user = FilamentUser::firstOrCreate(
+            [
+                'first_name' => 'Thorsten',
+                'last_name' => 'Ruppenstein',
+                'email' => 'thorsten@ddev.site',
+            ],
+            [
+                'first_name' => 'Thorsten',
+                'last_name' => 'Ruppenstein',
+                'email' => 'thorsten@ddev.site',
+                'password' => Hash::make('thorsten@ddev.site')
+            ]
+        );
+
+        $user->assignRole(RoleName::SUPER_ADMIN);
+        $user->save();
     }
 }
